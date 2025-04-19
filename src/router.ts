7add8ccd1af7ed0fccea4781e81a8c98fc9be6bc
FahmiDhika <fahmidhika810@gmail.com>
2025-04-20 05:54:08 +0700

    post using form data

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import { getAllMessage, newMessage } from "./controller"
 import { verifyNewMessage } from "./middleware"
+import multer from "multer"
 
+const upload = multer()
 const app = express()
 app.use(express.json())
 
 app.get(`/get`, getAllMessage)
-app.post(`/post`, [verifyNewMessage], newMessage)
+app.post(`/post`, [verifyNewMessage, upload.single("")], newMessage)
 
 export default app
