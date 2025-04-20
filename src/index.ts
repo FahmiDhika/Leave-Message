a5d02779e490ec77205ab59c4f383db9141b4fd1
FahmiDhika <fahmidhika810@gmail.com>
2025-04-20 19:11:22 +0700

    finalizing rest api project

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import express from "express"
-import cors from "cors"
 
 import messageRouter from "./router"
 
 const app = express()
-app.use(cors())
-
-const PORT = 4000
 
 app.use(`/message`, messageRouter)
 
-app.listen(PORT, () => {
-    console.log(`Server berjalan di port ${PORT}`)
+app.listen(4000, () => {
+    console.log(`Server berjalan di port 4000`)
 })
